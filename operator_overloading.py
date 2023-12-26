fe2d8a4e5d9dd853915803f81edb4c2bd7fff87c
Baber Ibrar <61284156+baberibrar@users.noreply.github.com>
2023-12-26 11:32:34 +0000

    ➕ Calculate total pages in books using __add__ method

--- operator_overloading.py
+++ operator_overloading.py
@@ -1,9 +1,14 @@
-num1 = 20
-num2 = 10
+class Book:
+    def __init__(self, title, pages) -> None:
+        self.title = title
+        self.pages = pages
+        
+    def __add__(self, other):
+        total = self.pages + other.pages
+        return total
+        
+b1 = Book('Unknown Soul', 300)
+b2 = Book('Road Story', 200)
 
-print(num1 + num2)
-print(num1.__add__(num2))
-
-print(int.__add__(num1, num2))
-
-print(dir(int))
+total_pages = b1 + b2
+print("The total number of pages in both books is: ", total_pages)
